23bb4f27efc22885e9d151ec8bf3ae4552c96c37
Vaibhav Honnikere <vaibhavhonnikere00@gmail.com>
2026-04-12 12:04:48 +0530

    fix: dummy embeddings for ingest

--- app/embeddings/embedder.py
+++ app/embeddings/embedder.py
@@ -1,9 +1,11 @@
 from typing import List
+import numpy as np
+
 
 class Embedder:
     def __init__(self):
         pass
 
     def embed_texts(self, texts: List[str]):
-        # Return dummy embeddings (dimension = 384)
-        return [[0.0]*384 for _ in texts]
+        # Dummy embeddings (for deployment)
+        return np.random.rand(len(texts), 384).astype("float32")
